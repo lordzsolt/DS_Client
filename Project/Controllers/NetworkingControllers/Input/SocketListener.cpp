0ddc0997212a0db41b98872bcc442bfd268eff36
Lord Zsolt <Lordzsoltt@gmail.com>
2015-01-03 18:49:37 +0200

    Implemented test socket listening.

--- Project/Controllers/NetworkingControllers/Input/SocketListener.cpp
+++ Project/Controllers/NetworkingControllers/Input/SocketListener.cpp
@@ -1,14 +1,62 @@
 #include "SocketListener.h"
 
 SocketListener::SocketListener(std::string serverAddress, unsigned short port) {
-
     _socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (_socket == INVALID_SOCKET) {
+        //TODO: Add exception
         wprintf(L"socket failed with error: %ld\n", WSAGetLastError());
-        WSACleanup();
     }
+
     sockaddr_in recvAddr;
     recvAddr.sin_family = AF_INET;
     recvAddr.sin_port = htons(port);
     recvAddr.sin_addr.s_addr = inet_addr(serverAddress.c_str());
+
+    int iResult = connect(_socket, (SOCKADDR *)&recvAddr, sizeof (recvAddr));
+    if (iResult == SOCKET_ERROR) {
+        //TODO: Add exception
+        wprintf(L"connects to server failed with error: %d\n",WSAGetLastError());
+    }
+
+    startListening();
+}
+
+
+void SocketListener::startListening() {
+    u_long nonblockingMode = 1;
+
+    ioctlsocket(_socket, FIONBIO, &nonblockingMode);
+
+    fd_set readfds;
+    struct timeval timeValues;
+
+    //clear the set
+    FD_ZERO(&readfds);
+    // add our descriptors to the set
+    FD_SET(_socket, &readfds);
+
+    timeValues.tv_sec = 10;
+    timeValues.tv_usec = 500000;
+    int iResult = select(0, &readfds, NULL, NULL, &timeValues);
+
+    if (iResult == SOCKET_ERROR) {
+        wprintf(L"Select failed with error: %d\n",WSAGetLastError());
+    }
+    else {
+        if (iResult == 0) {
+            printf("Timeout occurred!  No data after 10.5 seconds.\n");
+        }
+        else {
+            if (FD_ISSET(_socket, &readfds)) {
+                readSocket();
+                printf("Data was received\n");
+            }
+        }
+        startListening();
+    }
+}
+
+
+void SocketListener::readSocket() {
+    wprintf(L"Something actually happened: %d\n",WSAGetLastError());
 }
--- Project/main.cpp
+++ Project/main.cpp
@@ -1,20 +1,30 @@
 #include "Constants/ProtocolConstants.h"
 #include "Controllers/Session.h"
+#include "Controllers/NetworkingControllers/Networking.h"
 #include "Models/MessageModels/Messages/LoginMessage.h"
 #include "Models/MessageModels/Messages/RegisterMessage.h"
 
 #include <iostream>
+#include <iomanip>
+#include <unistd.h>
+
 
 using namespace std;
 
 int main() {
-    Session activeSession();
-    std::string username("Hello");
-    std::string password("Szia");
-    RegisterMessage message(0,MessageTag::New, username, password);
-    cout << message.serialize();
-//    if (!activeSession.login()) {
-//
-//    }
+
+    Session activeSession;
+    string password = "123456";
+    string username;
+    Networking networking = activeSession.getNetworking();
+    do {
+        cout << endl << "Username = ";
+        cin >> username;
+        NetworkingCallback lamdaCallback = [] (bool success) {
+            cout << "Success";
+        };
+        networking.login(username, password, lamdaCallback);
+    } while (username != "X");
+
     return 0;
 }
