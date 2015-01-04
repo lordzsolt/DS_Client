603593b841cacfe5d7347312b3d8e333b324f7f7
Lord Zsolt <Lordzsoltt@gmail.com>
2015-01-04 05:37:16 +0200

    Implemented SocketFactory.

---
+++ Project/Factories/SocketFactory.cpp
@@ -0,0 +1,19 @@
+#include "SocketFactory.h"
+
+SOCKET SocketFactory::createSocket(std::string address, unsigned short port) {
+    SOCKET soc = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (soc == INVALID_SOCKET) {
+        //TODO: Add some exception
+    }
+
+    sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(port);
+    addr.sin_addr.s_addr = inet_addr(address.c_str());
+
+    int iResult = connect(soc, (SOCKADDR *)&addr, sizeof(addr));
+    if (iResult == SOCKET_ERROR) {
+        //TODO: Add some exception
+    }
+    return soc;
+}
--- Project/Factories/SocketFactory.h
+++ Project/Factories/SocketFactory.h
@@ -1,24 +1,7 @@
+#include <winsock.h>
+#include <string>
 
-
-static class SocketFactory {
+class SocketFactory {
 public:
-    static SOCKET createSocket(std::string address, unsigned short port) {
-        SOCKET socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-        if (socket == INVALID_SOCKET) {
-            //TODO: Add some exception
-            wprintf(L"socket failed with error: %ld\n", WSAGetLastError());
-        }
-
-        sockaddr_in address;
-        address.sin_family = AF_INET;
-        address.sin_port = htons(port);
-        address.sin_addr.s_addr = inet_addr(serverAddress.c_str());
-
-        int iResult = connect(socket, (SOCKADDR *)&address, sizeof(address));
-        if (iResult == SOCKET_ERROR) {
-            wprintf(L"Connect to server failed with error: %d\n", WSAGetLastError());
-            //TODO: Add some exception
-        }
-        return socket;
-    }
+    static SOCKET createSocket(std::string address, unsigned short port);
 };
